d834682e220cbea5e088e3719beba4d3cecfca05
Mateusz Lawczys <ma6tek@gmail.com>
2017-12-28 22:54:01 +0100

    Fix js

--- js/scripts.js
+++ js/scripts.js
@@ -83,8 +83,8 @@ $(function() {
     }
     $('.create-column')
     .click(function(){
-      var name = prompt('Enter a column name');
-      var column = new Column(name);
+        var name = prompt('Enter a column name');
+        var column = new Column(name);
           board.addColumn(column);
     });
     // CREATING COLUMNS
